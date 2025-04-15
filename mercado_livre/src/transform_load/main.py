f5f09f32b36ca80ac74c7065965d1a8e608554ec
flourenco-daniel <danielferreiralourenco95@gmail.com>
2025-04-15 18:40:27 -0300

    começando a tratar os dados

--- mercado_livre/src/transform_load/main.py
+++ mercado_livre/src/transform_load/main.py
@@ -6,5 +6,12 @@ import os
 path = "mercado_livre/data/data.json"
 df = pd.read_json(path)
 
-print(df.head())
+#setando pandas pra mostrar todas as colunas
+pd.options.display.max_columns = None
 
+
+df['_source'] = "https://lista.mercadolivre.com.br/notebook?sb=rb#D[A:notebook]"
+df['_datetime'] = datetime.now()
+
+
+print(df.head())
