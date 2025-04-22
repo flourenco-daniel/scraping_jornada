5422a77ee30d3257aa61ba1370de609041c696c1
Daniel Lourenco <danielferreiralourenco95@gmail.com>
2025-04-22 15:41:48 -0300

    subindo pro banco

--- mercado_livre/src/transform_load/main.py
+++ mercado_livre/src/transform_load/main.py
@@ -10,8 +10,33 @@ df = pd.read_json(path)
 pd.options.display.max_columns = None
 
 
+
+df['old_value'] = df['old_value'].fillna('0')
+df['new_value'] = df['new_value'].fillna('0')
+df['rating'] = df['rating'].fillna('0')
+
+df['old_value'] = df['old_value'].astype(str).str.replace('.', '', regex=False)
+df['new_value'] = df['new_value'].astype(str).str.replace('.', '', regex=False)
+df['rating'] = df['rating'].astype(str).str.replace('[\(\)]', '', regex=True)
+
 df['_source'] = "https://lista.mercadolivre.com.br/notebook?sb=rb#D[A:notebook]"
 df['_datetime'] = datetime.now()
+df['old_value'] = df['old_value'].astype(float)
+df['new_value'] = df['new_value'].astype(float)
+df['rating'] = df['rating'].astype(float)
 
 
-print(df.head())
+df = df[
+    (df['old_value'] >=1000) & (df['old_value'] <= 10000) &
+    (df['old_value'] >=1000) & (df['old_value'] <= 10000)
+]
+
+print(df)
+
+#salvando no banco
+
+conn = sqlite3.connect('mercado_livre/data/notebook.sql')
+
+df.to_sql('notebook_research', conn, if_exists='replace', index=False)
+
+conn.close()
